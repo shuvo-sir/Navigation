59b8ead3cb95db8879773afd93ca80da01f9b968
Shuvo Halder <shuvohalder499@gmail.com>
2025-06-25 21:31:36 +0600

    Create a stack navigator

---
+++ app/App.js
@@ -0,0 +1,16 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "../screens/HomeScreen";
+import AboutScreen from "../screens/AboutScreen";
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen}/>
+        <Stack.Screen name="About" component={AboutScreen} />
+      </Stack.Navigator>
+    
+  );
+}
---
+++ screens/AboutScreen.js
@@ -0,0 +1,22 @@
+import { Text, View, StyleSheet } from "react-native";
+
+export default function AboutScreen() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>Home Screen</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    text: {
+        fontSize: 24,
+        fontWeight: "bold",
+        marginBottom: 16,
+    },
+})
