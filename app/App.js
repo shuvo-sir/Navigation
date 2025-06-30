875f0881c5c9c1757aab686ba2347b05d60499ff
Shuvo Halder <shuvohalder499@gmail.com>
2025-06-30 22:33:25 +0600

    change the icon color

--- app/App.js
+++ app/App.js
@@ -3,6 +3,7 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
 import ProfileScreen from "../screens/ProfileScreen";
 import CourseListScreen from "../screens/CourseListScreen";
 import SettingsScreen from "../screens/SettingsScreen";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 
 const Tab = createBottomTabNavigator()
@@ -11,11 +12,21 @@ export default function App() {
     return (
         <Tab.Navigator 
             screenOptions={{
-                tabBarActiveTintColor: "purple"
+                tabBarActiveTintColor: "purple",
+                tabBarInactiveTintColor: "blck"
             }}
         >
             <Tab.Screen name="Course List" component={CourseListScreen}/>
-            <Tab.Screen name="Profile" component={ProfileScreen}/>
+            <Tab.Screen 
+                name="Profile" 
+                component={ProfileScreen}
+                options={{
+                    tabBarLabel: "My profile",
+                    tabBarIcon: ({color}) => (
+                        <Ionicons name="person" size={20} color={color}/>
+                    )
+                }}
+            />
             <Tab.Screen name="Setting"component={SettingsScreen}/>
         </Tab.Navigator>
     )
